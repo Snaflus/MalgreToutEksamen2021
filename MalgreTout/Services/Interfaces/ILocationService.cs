af5de52bd2aa6fd037fb86528ecd2c210d2e2a25
Riis <Riis@RIIS-5700U>
2021-11-23 11:03:55 +0100

    Add project files.

---
+++ MalgreTout/Services/Interfaces/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using MalgreTout.Models;
+
+namespace MalgreTout.Services.Interfaces
+{
+    public interface ILocationService
+    {
+        //IEnumerable<Location> GetLocation(string filter);
+        //IEnumerable<Location> GetLocation();
+    }
+}
