a290a8b832b2260a55de6b94ca65a612a60a52e5
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-11-29 10:43:09 +0100

    SQL database færdiggørelse

--- MalgreTout/SQL Queries.sql
+++ MalgreTout/SQL Queries.sql
@@ -1,5 +1,4 @@
-﻿--drop table Telefonnumre
---drop table Emails
+﻿--drop table Emails
 --drop table Antal_magasiner
 --drop table Kontaktpersoner
 --drop table Åbningstider
@@ -13,49 +12,39 @@ create table Postnumre(
 );
 
 create table Lokationer(
-	ID int identity(1, 1) not null,
+	Lokation_ID int identity(1, 1) not null,
 	Firmanavn varchar(30) not null,
-	Addresse varchar(30) not null,
+	Adresse varchar(30) not null,
 	Postnummer int not null,
-	primary key (ID),
+	Telefonnummer varchar (15),
+	primary key (Lokation_ID),
 	foreign key (Postnummer) references	Postnumre(Postnummer),
 );
 
 create table Åbningerstider(
-	ID int not null,
+	Lokation_ID int not null,
 	Åbningstider varchar(10) not null,
-	primary key (ID),
-	foreign key (ID) references Lokationer(ID),
+	primary key (Lokation_ID),
+	foreign key (Lokation_ID) references Lokationer(Lokation_ID) on delete cascade,
 );
-
-create table Telefonnumre(
-	ID int not null,
-	Telefonnummer varchar (15) not null,
-	primary key (ID),
-	foreign key (ID) references Lokationer(ID),
-);
-
-create table Emails(
-	ID int not null,
-	Email varchar (50) not null,
-	primary key (ID),
-	foreign key (ID) references Lokationer(ID),
-);
-
 
 create table Antal_magasiner(
-	ID int not null,
-	Antal_magasiner int not null,
-	primary key (ID),
-	foreign key (ID) references Lokationer(ID),
+	Lokation_ID int not null,
+	Magasin_ID int not null,
+	Leveret_magasiner int,
+	Afhentet_magasiner int,
+	primary key (Lokation_ID),
+	foreign key (Lokation_ID) references Lokationer(Lokation_ID) on delete cascade,
 );
 
-
 create table Kontaktpersoner(
-	ID int not null,
+	Lokation_ID int not null,
+	Kontakt_ID int identity(1,1) not null,
 	Kontaktperson varchar (40) not null,
-	primary key (ID),
-	foreign key (ID) references Lokationer(ID),
+	Email varchar (50) not null,
+	Telefonnummer varchar(15),
+	primary key (Kontakt_ID),
+	foreign key (Lokation_ID) references Lokationer(Lokation_ID) on delete cascade,
 );
 
 --create table Koordinater (
@@ -72,35 +61,23 @@ Insert into Postnumre values (8500,'Grenå');
 Insert into Postnumre values (9800,'Hjøring');
 Insert into Postnumre values (7500,'Holstrebro');
 
-Insert into Lokationer values ('Land og Fritid Bjerringbro','Jørgens Allé 33', 8850);
-Insert into Lokationer values ('Land og Fritid Bårse','Korndrevet 4', 4720);
-Insert into Lokationer values ('Land og Fritid Grenå','Djurslandskajen 1', 8500);
-Insert into Lokationer values ('Land og Fritid Hjørring','Johs. E Rasmussens Vej 1', 9800);
-Insert into Lokationer values ('Land og Fritid Holstebro','Mads Bjerres Vej 2', 7500);
+Insert into Lokationer values ('Land og Fritid Bjerringbro','Jørgens Allé 33', 8850, '12345671');
+Insert into Lokationer values ('Land og Fritid Bårse','Korndrevet 4', 4720, '12345672');
+Insert into Lokationer values ('Land og Fritid Grenå','Djurslandskajen 1', 8500, '12345673');
+Insert into Lokationer values ('Land og Fritid Hjørring','Johs. E Rasmussens Vej 1', 9800, '12345674');
+Insert into Lokationer values ('Land og Fritid Holstebro','Mads Bjerres Vej 2', 7500, '12345675');
 
-Insert into Telefonnumre values (1,'12345671');
-Insert into Telefonnumre values (2,'12345672');
-Insert into Telefonnumre values (3,'12345673');
-Insert into Telefonnumre values (4,'12345674');
-Insert into Telefonnumre values (5,'12345675');
+Insert into Kontaktpersoner values (1,'Lisa Holst Nicolajsen','info-bjerringbro@landogfritid.dk','12345671');
+Insert into Kontaktpersoner values (2,'Camilla S. Jørgensen','info-præstø@landogfritid.dk','12345672');
+Insert into Kontaktpersoner values (3,'Anette Alsner','info-grenå@landogfritid.dk','12345673');
+Insert into Kontaktpersoner values (4,'Charlotte Bach Olsen','info-hjøring@landogfritid.dk','12345674');
+Insert into Kontaktpersoner values (5,'Nicholaj Simonsen','info-holstebro@landogfritid.dk','12345675');
 
-Insert into Kontaktpersoner values (1,'Lisa Holst Nicolajsen');
-Insert into Kontaktpersoner values (2,'Camilla S. Jørgensen');
-Insert into Kontaktpersoner values (3,'Anette Alsner');
-Insert into Kontaktpersoner values (4,'Charlotte Bach Olsen');
-Insert into Kontaktpersoner values (5,'Nicholaj Simonsen');
-
-Insert into Emails values (1,'info-bjerringbro@landogfritid.dk');
-Insert into Emails values (2,'info-præstø@landogfritid.dk');
-Insert into Emails values (3,'info-grenå@landogfritid.dk');
-Insert into Emails values (4,'info-hjøring@landogfritid.dk');
-Insert into Emails values (5,'info-holstebro@landogfritid.dk');
-
-Insert into Antal_magasiner values (1,100);
-Insert into Antal_magasiner values (2,150);
-Insert into Antal_magasiner values (3,150);
-Insert into Antal_magasiner values (4,100);
-Insert into Antal_magasiner values (5,100);
+Insert into Antal_magasiner values (1,49,100,0);
+Insert into Antal_magasiner values (2,49,150,140);
+Insert into Antal_magasiner values (3,49,150,170);
+Insert into Antal_magasiner values (4,49,100,110);
+Insert into Antal_magasiner values (5,49,100,90);
 
 Insert into Åbningerstider values (1,'10-18');
 Insert into Åbningerstider values (2,'10-18');
