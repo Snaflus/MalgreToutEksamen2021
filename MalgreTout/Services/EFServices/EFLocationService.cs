67f224ac24e3f5d4bce4cff3207e5058defb934d
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-11-29 11:44:51 +0100

    Services og interfaces for SQL tabel <Lokationer>

--- MalgreTout/Services/EFServices/EFLocationService.cs
+++ MalgreTout/Services/EFServices/EFLocationService.cs
@@ -2,14 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using MalgreTout.Models;
+using MalgreTout.Models;
 using MalgreTout.Services.Interfaces;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace MalgreTout.Services.EFServices
 {
     public class EFLocationService:ILocationService
     {
-        //private LocationDBContext context;
+        MalgretoutContext context;
+        public EFLocationService(MalgretoutContext service)
+        {
+            context = service;
+        }
+
+        public IEnumerable<Lokationer> GetLocations(string filter)
+        {
+            return this.context.Set<Lokationer>().Where(s => s.Firmanavn.StartsWith(filter)).AsNoTracking().ToList();
+        }
+
+        public IEnumerable<Lokationer> GetLocations()
+        {
+            return context.Lokationers;
+        }
+
+        public void AddLocation(Lokationer location)
+        {
+            context.Lokationers.Add(location);
+            context.SaveChangesAsync();
+        }
+
+        public void DeleteLocation(Lokationer location)
+        {
+            context.Lokationers.Remove(location);
+            context.SaveChangesAsync();
+        }
+
+        public Lokationer GetLocationById(int id)
+        {
+            return context.Lokationers.Find(id);
+        }
+
     }
 }
--- MalgreTout/Services/Interfaces/ILocationService.cs
+++ MalgreTout/Services/Interfaces/ILocationService.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using MalgreTout.Models;
+using MalgreTout.Models;
+using MalgreTout.Services.EFServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace MalgreTout.Services.Interfaces
 {
     public interface ILocationService
     {
-        //IEnumerable<Location> GetLocation(string filter);
-        //IEnumerable<Location> GetLocation();
+        IEnumerable<Lokationer> GetLocations(string filter);
+        IEnumerable<Lokationer> GetLocations();
+        void AddLocation(Lokationer location);
+        void DeleteLocation(Lokationer location);
+        Lokationer GetLocationById(int id);
     }
 }
