0635b462eb739a814f59828316d490fc25e7a49c
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-12-06 12:07:44 +0100

    CSS og HTML ændringer, samt delvis udførsel af Create feature

--- MalgreTout/Pages/DistributionPoints/Create.cshtml.cs
+++ MalgreTout/Pages/DistributionPoints/Create.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MalgreTout.Models;
+using MalgreTout.Services.Interfaces;
 
 namespace MalgreTout.Pages.DistributionPoints
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public DistributionPoint DistributionPoint { get; set; } = new DistributionPoint();
+        public void OnGet(int id)
         {
+            DistributionPoint.LocationId = id;
+        }
+        IDistributionService distributionService;
+        public CreateModel(IDistributionService service)
+        {
+            this.distributionService = service;
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            distributionService.AddDistributionPoint(DistributionPoint);
+            return RedirectToPage("GetDistributionPoints");
         }
     }
 }
