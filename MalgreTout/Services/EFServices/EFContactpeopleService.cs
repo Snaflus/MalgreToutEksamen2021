06228fba963e9c37c2f69198e7e3db0c3dd96e9f
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-12-13 10:45:17 +0100

    Filnavn ændring

---
+++ MalgreTout/Services/EFServices/EFContactpeopleService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MalgreTout.Services.EFServices
+{
+    public class EFContactpeopleService
+    {
+    }
+}
