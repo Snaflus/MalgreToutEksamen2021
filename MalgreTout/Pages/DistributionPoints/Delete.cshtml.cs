d1f2ad56111ff82eb39d3f12394300f12e9e487f
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-12-16 11:23:53 +0100

    Update og delete færdiggjort

--- MalgreTout/Pages/DistributionPoints/Delete.cshtml.cs
+++ MalgreTout/Pages/DistributionPoints/Delete.cshtml.cs
@@ -12,16 +12,26 @@ namespace MalgreTout.Pages.DistributionPoints
     public class DeleteModel : PageModel
     {
         [BindProperty]
-        public DistributionPoint DistributionPoint { get; set; }        
-        IDistributionService distributionService;       
-        public DeleteModel(IDistributionService service)
+        public DistributionPoint DistributionPoint { get; set; }
+        [BindProperty]
+        public Contactperson Contactperson { get; set; }
+        [BindProperty]
+        public OpeningHour OpeningHour { get; set; }
+        IDistributionService distributionService;
+        IContactPeopleService contactPeopleService;
+        IOpeningHourService openingHourService;
+        public DeleteModel(IDistributionService service1, IContactPeopleService service2, IOpeningHourService service3)
         {
-            this.distributionService = service;
+            this.distributionService = service1;
+            this.contactPeopleService = service2;
+            this.openingHourService = service3;
             DistributionPoint = new DistributionPoint();
         }
         public void OnGet(int id)
         {
-            DistributionPoint = distributionService.GetDistributionPointById(id);       
+            DistributionPoint = distributionService.GetDistributionPointById(id);
+            Contactperson = contactPeopleService.GetContactpersonByLocationId(id);
+            OpeningHour = openingHourService.GetOpeningHourByLocationId(id);
         }
         public IActionResult OnPost()
         {
