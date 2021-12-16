31cd7ce969f0c779bb050750e4da0699fa93b3f4
Casper Riis Lie Nielsen <casp909i@edu.easj.dk>
2021-12-16 12:39:05 +0100

    GetMagasine færdig

---
+++ MalgreTout/Services/EFServices/EFMagasineService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MalgreTout.Models;
+using MalgreTout.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MalgreTout.Services.EFServices
+{
+    public class EFMagasineService : IMagasineService
+    {
+        MalgretoutContext context;
+        public EFMagasineService(MalgretoutContext service)
+        {
+            context = service;
+        }
+        public IEnumerable<NoOfMagazine> GetMagazines(int filter)
+        {
+            return context.NoOfMagazines                
+                .Include(s => s.Location)
+                .Include(c => c.Location.Contactpeople)
+                .Include(v => v.Location.ZipcodeNavigation)
+                .Include(i => i.Location.OpeningHours)
+                .Where(s => s.MagazineIssue.Equals(filter)).AsNoTracking().ToList();
+        }
+
+        public IEnumerable<NoOfMagazine> GetMagazines()
+        {
+            return context.NoOfMagazines
+                .Include(s => s.Location)
+                .Include(c => c.Location.Contactpeople)
+                .Include(v => v.Location.ZipcodeNavigation)
+                .Include(i => i.Location.OpeningHours);
+        }
+        public void AddMagasine(NoOfMagazine magasine)
+        {
+            context.NoOfMagazines.Add(magasine);
+            context.SaveChanges();
+        }
+
+        public void DeleteMagasine(NoOfMagazine magasine)
+        {
+            context.NoOfMagazines.Remove(magasine);
+            context.SaveChanges();
+        }
+
+        public void UpdateMagasine(NoOfMagazine magasine)
+        {
+            context.NoOfMagazines.Update(magasine);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MalgreTout/Services/Interfaces/IMagasineService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MalgreTout.Models;
+using MalgreTout.Services.EFServices;
+using Microsoft.EntityFrameworkCore;
+
+namespace MalgreTout.Services.Interfaces
+{
+    public interface IMagasineService
+    {
+        IEnumerable<NoOfMagazine> GetMagazines(int filter);
+        IEnumerable<NoOfMagazine> GetMagazines();
+        void AddMagasine(NoOfMagazine magasine);
+        void DeleteMagasine(NoOfMagazine magasine);
+        void UpdateMagasine(NoOfMagazine magasine);
+    }
+}
